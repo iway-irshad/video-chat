e643e92c30543aa1ce0e7a8a2e19420ea244bd17
iwayIrshad <iway.app7@gmail.com>
2025-10-16 01:15:26 +0530

    Implement onboarding features with middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,3 +113,50 @@ export const logout = (_, res) => {
     });
     return res.status(200).json({ message: "Logout successful" });
 };
+
+export const onboard = async (req, res) => {
+    try {
+        const userId = req.user._id;
+    
+        const { fullName, bio, nativeLanguage, learningLanguage, location } = req.body;
+    
+        if (!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+          return res.status(400).json({
+            message: "All fields are required",
+            missingFields: [
+              !fullName && "fullName",
+              !bio && "bio",
+              !nativeLanguage && "nativeLanguage",
+              !learningLanguage && "learningLanguage",
+              !location && "location",
+            ].filter(Boolean),
+          });
+        }
+    
+        const updatedUser = await User.findByIdAndUpdate(
+          userId,
+          {
+            ...req.body,
+            isOnboarded: true,
+          },
+          { new: true }
+        );
+    
+        if (!updatedUser) return res.status(404).json({ message: "User not found" });
+    
+        try {
+          await upsertStreamUser({
+            id: updatedUser._id.toString(),
+            name: updatedUser.fullName,
+            image: updatedUser.profilePic || "",
+          });
+        } catch (streamError) {
+          console.log("Error updating Stream user during onboarding:", streamError.message);
+        }
+    
+        res.status(200).json({ success: true, user: updatedUser });
+      } catch (error) {
+        console.error("Onboarding error:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+}
