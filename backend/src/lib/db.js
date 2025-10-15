f35e95903b1dcab5712de10e55cad7589ac2c0fd
iwayIrshad <iway.app7@gmail.com>
2025-10-15 22:56:28 +0530

    Add MongoDB connection and Stream

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+    try {
+        if (!ENV.MONGO_URI) throw new Error("MONGO_URI is not set");
+        const conn = await mongoose.connect(ENV.MONGO_URI);
+        console.log("MongoDB connected: ", conn.connection.host);
+    } catch (error) {
+        console.error("MongoDB connection error: ", error);
+        process.exit(1); // 1 status code means failure, 0 means success
+    }
+}
--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,5 +1,9 @@
 import 'dotenv/config';
 
 export const ENV = {
-    PORT: process.env.PORT || "5001"
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    NODE_ENV: process.env.NODE_ENV,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
 };
