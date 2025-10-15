9d2e76058135b2b581dac20a6c991f51c5560299
iwayIrshad <iway.app7@gmail.com>
2025-10-16 00:04:10 +0530

    Login and logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,10 +64,43 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = async (_, res) => {
-    res.send("Login controller");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // never tell the client which one is incorrect: password or email
+        // for security reasons
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user._id, res);
+        return res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+    } catch (error) {
+        console.error("Error in login controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const logout = (_, res) => {
-    res.send("Logout controller");
+    res.cookie("jwt", "", {
+        maxAge: 0,
+        httpOnly: true
+    });
+    return res.status(200).json({ message: "Logout successful" });
 };
