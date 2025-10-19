95791ba68a9a90a86db5401e340006b0ce544c4a
iwayIrshad <iway.app7@gmail.com>
2025-10-19 06:30:30 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -11,6 +12,8 @@ import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 const PORT = ENV.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,8 +26,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-// Ensure PORT is defined
-if (!PORT) throw new Error("PORT is not defined in environment variables");
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 
 connectDB().then(() => {
   app.listen(PORT, () => {
